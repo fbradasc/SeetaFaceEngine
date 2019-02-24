1b45fa21549769553fce6d413691821e24d44f1d
fbradasc@gmail.com <fbradasc@gmail.com>
2019-02-24 18:21:57 +0100

    WIP

--- FaceIdentification/src/test/test_face_recognizer.cpp
+++ FaceIdentification/src/test/test_face_recognizer.cpp
@@ -64,7 +64,7 @@ using namespace std;
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "face_identification.h"
 #include "common.h"
